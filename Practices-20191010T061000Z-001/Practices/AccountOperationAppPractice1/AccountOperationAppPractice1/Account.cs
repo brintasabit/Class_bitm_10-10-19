52bd3381525c1e1a0b7734e80cfecd2c0066fc78
brintasabit <brintasabit@gmail.com>
2019-10-10 12:55:48 +0600

    Class Exercise

---
+++ Practices-20191010T061000Z-001/Practices/AccountOperationAppPractice1/AccountOperationAppPractice1/Account.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountOperationAppPractice1
+{
+    public class Account
+    {
+        public string accountNumber { get; set; }
+        public string customerName { get; set; }
+
+        public int Deposit()
+        {
+            int dMoney=0;
+            return dMoney;
+        }
+
+        public int Withdraw()
+        {
+            int wMoney=0;
+            return wMoney;
+        }
+
+       /* public int Balance()
+        {
+            
+        }*/
+       
+    }
+}
---
+++ Practices-20191010T061000Z-001/Practices/AccountOperationAppPractice1/AccountOperationAppPractice1/AccountOperationUi.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AccountOperationAppPractice1
+{
+    public partial class AccountOperationUi : Form
+    {
+        public AccountOperationUi()
+        {
+            InitializeComponent();
+        }
+
+        Account account=new Account();
+        private void CreateButton_Click(object sender, EventArgs e)
+        {
+            account.accountNumber=accountNoTextBox.Text;
+            account.customerName =customerNameTextBox.Text ;
+            MessageBox.Show("Account Created!");
+        }
+
+        private void DipositButton_Click(object sender, EventArgs e)
+        {
+            account.Deposit();
+            MessageBox.Show("Deposited!");
+        }
+
+        private void WithdrawButton_Click(object sender, EventArgs e)
+        {
+            account.Withdraw();
+            MessageBox.Show("Withdrawn!");
+        }
+
+        private void ReportButton_Click(object sender, EventArgs e)
+        {
+            
+            MessageBox.Show(account.customerName+","+"Your Account Number: "+
+                            account.accountNumber+"& It's Balance: ");
+        }
+    }
+}
